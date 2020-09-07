c542aa22e4afddc19622ce532f49bac5559c9d4d
Yu Wu <ywuu0096@student.monash.edu>
2020-09-07 15:16:12 +0800

    添加项目文件。

---
+++ Iteration 2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Iteration_2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Iteration 2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Iteration_2.WebApiApplication" Language="C#" %>
